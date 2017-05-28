38c5657e1e6dc3dedbf999ef05b73901053a79b7
Ademola Adegbuyi <ooade96@gmail.com>
2017-05-28 13:33:48 +0100

    Fix SW #32

--- next.config.js
+++ next.config.js
@@ -1,34 +1,48 @@
+const path = require('path');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 
 module.exports = {
-  webpack: (config, { dev }) => {
-    /* Enable only in Production */
-    if (!dev) {
-      // Service Worker
-      config.plugins.push(
-        new SWPrecacheWebpackPlugin({
-          filename: 'sw.js',
-          minify: true,
-          staticFileGlobsIgnorePatterns: [/\.next\//],
-          staticFileGlobs: [
-            'static/**/*' // Precache all static files by default
-          ],
-          forceDelete: true,
-          runtimeCaching: [
-            // Example with different handlers
-            {
-              handler: 'fastest',
-              urlPattern: /[.](png|jpg|css)/
-            },
-            {
-              handler: 'networkFirst',
-              urlPattern: /^http.*/ //cache all files
-            }
-          ]
-        })
-      );
-    }
+	webpack: (config, { dev }) => {
+		/**
+     * Install and Update our Service worker 
+     * on our main entry file :)
+     * Reason: https://github.com/ooade/NextSimpleStarter/issues/32
+     */
+		const oldEntry = config.entry;
 
-    return config;
-  }
-}
+		config.entry = () =>
+			oldEntry().then(entry => {
+				entry['main.js'].push(path.resolve('./utils/offline'));
+				return entry;
+			});
+
+		/* Enable only in Production */
+		if (!dev) {
+			// Service Worker
+			config.plugins.push(
+				new SWPrecacheWebpackPlugin({
+					filename: 'sw.js',
+					minify: true,
+					staticFileGlobsIgnorePatterns: [/\.next\//],
+					staticFileGlobs: [
+						'static/**/*' // Precache all static files by default
+					],
+					forceDelete: true,
+					runtimeCaching: [
+						// Example with different handlers
+						{
+							handler: 'fastest',
+							urlPattern: /[.](png|jpg|css)/
+						},
+						{
+							handler: 'networkFirst',
+							urlPattern: /^http.*/ //cache all files
+						}
+					]
+				})
+			);
+		}
+
+		return config;
+	}
+};

44e112c54b89a7644e0d26f2ccaabadfd5a55226
Ademola Adegbuyi <ooade96@gmail.com>
2019-01-12 11:34:00 +0100

    Fix #83: Fix insecure requests on Firefox

--- pages/_document.js
+++ pages/_document.js
@@ -11,10 +11,6 @@ export default class MyDocument extends Document {
 						content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,minimal-ui"
 					/>
 					<meta name="theme-color" content="#673ab7" />
-					<meta
-						httpEquiv="Content-Security-Policy"
-						content="upgrade-insecure-requests"
-					/>
 					<link rel="manifest" href="static/manifest.json" />
 					<link rel="icon" href="static/img/favicon.ico" />
 					<link

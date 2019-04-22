f690de1335ce5d344f0405e6c7643df718455f96
Ademola Adegbuyi <ooade96@gmail.com>
2019-04-22 05:19:52 +0100

    Add service worker destination

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const withOffline = require('next-offline')
 
 module.exports = withOffline({
+	target: 'serverless',
 	workboxOpts: {
+		swDest: 'static/service-worker.js',
 		runtimeCaching: [
 			{
 				urlPattern: /[.](png|jpg|ico|css)/,

d654c20f0db192e9c4ae980a24ff334a1a9c01f0
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-22 01:46:50 +0100

    switch from next-offline to next/pwa

--- next.config.js
+++ next.config.js
@@ -1,26 +1,34 @@
-const withOffline = require('next-offline')
+// const withOffline = require('next-offline');
 
-module.exports = withOffline({
-	workboxOpts: {
-		swDest: 'static/service-worker.js',
-		runtimeCaching: [
-			{
-				urlPattern: /[.](png|jpg|ico|css)/,
-				handler: 'CacheFirst',
-				options: {
-					cacheName: 'assets-cache',
-					cacheableResponse: {
-						statuses: [0, 200],
-					},
-				},
-			},
-			{
-				urlPattern: /^http.*/,
-				handler: 'NetworkFirst',
-				options: {
-					cacheName: 'http-cache',
-				},
-			},
-		],
-	},
+const withPwa= require('next-pwa')
+
+
+
+module.exports = withPwa({
+	dest: 'public',
+	register:true,
+	skipWaiting:true,
+	disable: process.env.NODE_ENV === 'development'
+	// workboxOpts: {
+	// 	swDest: 'static/service-worker.js',
+	// 	runtimeCaching: [
+	// 		{
+	// 			urlPattern: /[.](png|jpg|ico|css)/,
+	// 			handler: 'CacheFirst',
+	// 			options: {
+	// 				cacheName: 'assets-cache',
+	// 				cacheableResponse: {
+	// 					statuses: [0, 200],
+	// 				},
+	// 			},
+	// 		},
+	// 		{
+	// 			urlPattern: /^http.*/,
+	// 			handler: 'NetworkFirst',
+	// 			options: {
+	// 				cacheName: 'http-cache',
+	// 			},
+	// 		},
+	// 	],
+	// },
 })

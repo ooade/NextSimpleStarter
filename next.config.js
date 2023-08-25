7fa1f36f4b3538e9019e477e737872d9c54a04e2
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-25 03:42:13 +0000

    restore caching options in next.config.js

--- next.config.js
+++ next.config.js
@@ -8,27 +8,26 @@ module.exports = withPwa({
 	dest: 'public',
 	register:true,
 	skipWaiting:true,
-	disable: process.env.NODE_ENV === 'development'
-	// workboxOpts: {
-	// 	swDest: 'static/service-worker.js',
-	// 	runtimeCaching: [
-	// 		{
-	// 			urlPattern: /[.](png|jpg|ico|css)/,
-	// 			handler: 'CacheFirst',
-	// 			options: {
-	// 				cacheName: 'assets-cache',
-	// 				cacheableResponse: {
-	// 					statuses: [0, 200],
-	// 				},
-	// 			},
-	// 		},
-	// 		{
-	// 			urlPattern: /^http.*/,
-	// 			handler: 'NetworkFirst',
-	// 			options: {
-	// 				cacheName: 'http-cache',
-	// 			},
-	// 		},
-	// 	],
-	// },
+	disable: process.env.NODE_ENV === 'development',
+	swDest: 'static/service-worker.js',
+	runtimeCaching: [
+		{
+			urlPattern: /[.](png|jpg|ico|css)/,
+			handler: 'CacheFirst',
+			options: {
+			cacheName: 'assets-cache',
+			cacheableResponse: {
+					tatuses: [0, 200],
+				},
+			},
+		},
+		{
+			urlPattern: /^http.*/,
+			handler: 'NetworkFirst',
+			options: {
+				cacheName: 'http-cache',
+			},
+		},
+	],
+	
 })

1515d9ba2428508d5da65109d266f203caaca60d
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-25 10:25:47 +0100

    Feature/next pwa (#201)
    * upgrade react and next
    
    * switch from next-offline to next/pwa
    
    * restore caching options in next.config.js
    
    * Update next.config.js
    
    * Update next.config.js
    
    * Update service worker script path
    
    * Update .gitignore
    
    * Update next.config.js
    
    ---------
    
    Co-authored-by: Ademola <ooade96@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,26 +1,28 @@
-const withOffline = require('next-offline')
+const withPWA = require('next-pwa');
 
-module.exports = withOffline({
-	workboxOpts: {
-		swDest: 'static/service-worker.js',
-		runtimeCaching: [
-			{
-				urlPattern: /[.](png|jpg|ico|css)/,
-				handler: 'CacheFirst',
-				options: {
-					cacheName: 'assets-cache',
-					cacheableResponse: {
-						statuses: [0, 200],
-					},
+module.exports = withPWA({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === 'development',
+	sw: '/service-worker.js',
+	runtimeCaching: [
+		{
+			urlPattern: /[.](png|jpg|ico|css)/,
+			handler: 'CacheFirst',
+			options: {
+			cacheName: 'assets-cache',
+			cacheableResponse: {
+					statuses: [0, 200],
 				},
 			},
-			{
-				urlPattern: /^http.*/,
-				handler: 'NetworkFirst',
-				options: {
-					cacheName: 'http-cache',
-				},
+		},
+		{
+			urlPattern: /^http.*/,
+			handler: 'NetworkFirst',
+			options: {
+				cacheName: 'http-cache',
 			},
-		],
-	},
-})
+		},
+	],
+});

0971c7d92ec4cb71c732d321aebf7007610a1689
Ademola <ooade96@gmail.com>
2023-08-25 10:19:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-	dest: 'static',
+	dest: 'public',
 	register: true,
 	skipWaiting: true,
 	disable: process.env.NODE_ENV === 'development',

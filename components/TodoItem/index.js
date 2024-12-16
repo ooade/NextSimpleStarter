c4f878e7296eae66429a3a814e9eaff98d408daa
Ademola Adegbuyi <ooade96@gmail.com>
2024-12-16 13:05:29 +0000

    chore: prettify js files

--- components/TodoItem/index.js
+++ components/TodoItem/index.js
@@ -12,16 +12,13 @@ const DeleteIcon = () => (
 		role="presentation"
 		width={24}
 		height={24}
-		fill='currentColor'
+		fill="currentColor"
 	>
 		<path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path>
 	</svg>
 )
 
-
-
 const TodoItem = ({ todo, updateTodo, removeTodo }) => {
-
 	return (
 		<li className={styles.todoItem}>
 			<label
